69b6dcb8b698700ef57e24d5a21b60045a210f19
HardCorePawn <hardcorepawn69@gmail.com>
2021-01-13 10:16:32 +1300

    Adding "Press any Key" prompt
    Adds a "press any key to continue..." prompt at end of script execution. 
    
    This will prevent console window from automatically closing in the case that the user is using "drag and drop" functionality to execute the scripts by dropping a .key or .wallet or .cipher file onto the script as opposed to running from the command line.

--- decrypt_multibit_classic_wallet_cipher.py
+++ decrypt_multibit_classic_wallet_cipher.py
@@ -53,6 +53,31 @@ md5    = hashlib.md5
 
 key_expander = aespython.key_expander.KeyExpander(256)
 
+def wait_key():
+    ''' Wait for a key press on the console and return it. '''
+    result = None
+    if os.name == 'nt':
+        import msvcrt
+        result = msvcrt.getch()
+    else:
+        import termios
+        fd = sys.stdin.fileno()
+
+        oldterm = termios.tcgetattr(fd)
+        newattr = termios.tcgetattr(fd)
+        newattr[3] = newattr[3] & ~termios.ICANON & ~termios.ECHO
+        termios.tcsetattr(fd, termios.TCSANOW, newattr)
+
+        try:
+            result = sys.stdin.read(1)
+        except IOError:
+            pass
+        finally:
+            termios.tcsetattr(fd, termios.TCSAFLUSH, oldterm)
+
+    return result
+
+
 def aes256_cbc_decrypt(ciphertext, key, iv):
     """decrypts the ciphertext using AES256 in CBC mode
 
@@ -221,3 +246,7 @@ if __name__ == '__main__':
             display_error(str(e))
             if not e.args[0].startswith('incorrect password'):
                 raise
+    
+    print("")
+    print("Press any key to continue...")
+    wait_key()
